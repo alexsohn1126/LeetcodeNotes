6a2c76b4e79c9f50b50d162984597dfbf28c8556
Alex Sohn <alexsohn1126@gmail.com>
2023-05-21 23:53:28 -0400

    Add a feature to show current leetcode link

--- popup.js
+++ popup.js
@@ -1,9 +1,25 @@
 // Get the textarea element and save button
+const problemNum = document.getElementById('problemNum');
 const input = document.getElementById('input');
 const saveBtn = document.getElementById('save');
 
+function onGotTab(tab){
+  console.log(tab);
+  if (tab.url.match(/https:\/\/leetcode\.com\/problems\/.*/)){
+    problemNum.innerHTML = tab.url;
+  } else {
+    problemNum.innerHTML = "Not in a leetcode site";
+  }
+}
+
+function onError(error){
+  console.log(`Error: ${error}`);
+}
+
 // Load saved text on page load
 window.onload = () => {
+  browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => onGotTab(tabs[0]), onError);
+
   browser.storage.sync.get('savedText').then((data) => {
     if (data.savedText) {
       input.value = data.savedText;
